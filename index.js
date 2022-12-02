5a6ce66f801b558fccd13a92daef5bc1a0b7b9fd
JUNZHAO <762818814@qq.com>
2022-12-02 16:53:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ var clinet=new EIP_SCANNER(ip, scan, i_size, o_size, i_path, o_path,c_path,readw
 setInterval(() => {
     console.log(rgdb);  
 }, 1000);
+//ethernet ip scanner 使用的端口
+const dgram = require('dgram');
+const udp = dgram.createSocket('udp4');
+udp.on('error', (err) => {
+	console.log(`服务器异常：\n${err.stack}`);
+});
+udp.on('message', (msg, rinfo) => {
+	let tar = rinfo.address;
+	let buf = clinet.ioparse(msg);
+	udp.send(buf, 2222, tar, (err) => {
+	})
+});
+
+udp.on('listening', () => {
+});
+
+udp.bind(2222);
